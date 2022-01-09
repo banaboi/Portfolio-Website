293463ecdb518851c922c931d9c13ae57a0b8b10
Luke Banicevic <60857954+banaboi@users.noreply.github.com>
2022-01-09 21:21:35 +1100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { Button } from "react-bootstrap";
+import Particles from "react-tsparticles";
+import useWindowDimensions from "./utilities/useWindowDimensions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import options from "../src/tsparticlesconfig";
+
+const App = () => {
+    const [counter, setCounter] = useState(0);
+    const { height, width } = useWindowDimensions();
+    return (
+        <div className="App">
+            <Particles options={options}></Particles>
+            <div className="headline">
+                <h1>Luke Banicevic</h1>
+                <h2>Software Engineer</h2>
+            </div>
+        </div>
+    );
+};
 
 export default App;
