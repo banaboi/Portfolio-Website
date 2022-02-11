ce50699667b43d6e9e812bf01aff8b03db3fee29
Luke Banicevic <60857954+banaboi@users.noreply.github.com>
2022-02-11 15:16:04 +1100

    Fixed styling

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -9,6 +9,7 @@ import "../styles/App.scss";
 import emailjs from "@emailjs/browser";
 import { Alert, Snackbar } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import FadeInSection from "./FadeInSection";
 
 const sendMail = (name: string, email: string, message: string): string => {
     if (name === "" || email === "" || message === "") return "warning";
@@ -93,101 +94,103 @@ const ContactMe = () => {
 
     return (
         <>
-            <Container id="contact" className="section contact-section">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "left",
-                    }}
-                >
-                    <span className="sub-heading">Send me a message </span>
-                    <Box sx={{ mt: 3 }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                                <TextField
-                                    className="text-field"
-                                    id="#name"
-                                    required
-                                    fullWidth
-                                    label="Name"
-                                    autoFocus
-                                    value={name}
-                                    onChange={handleChange}
-                                />
+            <FadeInSection props={{ children: undefined, delay: "1000ms" }}>
+                <Container id="contact" className="section contact-section">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "left",
+                        }}
+                    >
+                        <span className="sub-heading">Send me a message </span>
+                        <Box sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        className="text-field"
+                                        id="#name"
+                                        required
+                                        fullWidth
+                                        label="Name"
+                                        autoFocus
+                                        value={name}
+                                        onChange={handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        className="text-field"
+                                        id="#email"
+                                        required
+                                        fullWidth
+                                        label="Email Address"
+                                        value={email}
+                                        onChange={handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        className="text-field"
+                                        id="#message"
+                                        required
+                                        multiline
+                                        fullWidth
+                                        rows={8}
+                                        label="Message"
+                                        value={message}
+                                        onChange={handleChange}
+                                    />
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    className="text-field"
-                                    id="#email"
-                                    required
-                                    fullWidth
-                                    label="Email Address"
-                                    value={email}
-                                    onChange={handleChange}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    className="text-field"
-                                    id="#message"
-                                    required
-                                    multiline
-                                    fullWidth
-                                    rows={8}
-                                    label="Message"
-                                    value={message}
-                                    onChange={handleChange}
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            className="contact-button"
-                            fullWidth
-                            variant="outlined"
-                            sx={{ mt: 3, mb: 2 }}
-                            style={{
-                                color: "lime",
-                                borderColor: "lime",
-                            }}
-                            onClick={handleClick}
-                        >
-                            Send
-                        </Button>
-                        <Snackbar
-                            id="success-snackbar"
-                            open={openSuccess}
-                            autoHideDuration={3000}
-                            onClose={handleClose}
-                        >
-                            <Alert severity="success">
-                                Message sent successfully
-                            </Alert>
-                        </Snackbar>
-                        <Snackbar
-                            id="error-snackbar"
-                            open={openError}
-                            autoHideDuration={3000}
-                            onClose={handleClose}
-                        >
-                            <Alert severity="error">
-                                Something went wrong, contact administrator
-                            </Alert>
-                        </Snackbar>
-                        <Snackbar
-                            id="warning-snackbar"
-                            open={openWarning}
-                            autoHideDuration={3000}
-                            onClose={handleClose}
-                        >
-                            <Alert severity="warning">
-                                Please fill in all fields before sending
-                            </Alert>
-                        </Snackbar>
+                            <Button
+                                className="contact-button"
+                                fullWidth
+                                variant="outlined"
+                                sx={{ mt: 3, mb: 2 }}
+                                style={{
+                                    color: "lime",
+                                    borderColor: "lime",
+                                }}
+                                onClick={handleClick}
+                            >
+                                Send
+                            </Button>
+                            <Snackbar
+                                id="success-snackbar"
+                                open={openSuccess}
+                                autoHideDuration={3000}
+                                onClose={handleClose}
+                            >
+                                <Alert severity="success">
+                                    Message sent successfully
+                                </Alert>
+                            </Snackbar>
+                            <Snackbar
+                                id="error-snackbar"
+                                open={openError}
+                                autoHideDuration={3000}
+                                onClose={handleClose}
+                            >
+                                <Alert severity="error">
+                                    Something went wrong, contact administrator
+                                </Alert>
+                            </Snackbar>
+                            <Snackbar
+                                id="warning-snackbar"
+                                open={openWarning}
+                                autoHideDuration={3000}
+                                onClose={handleClose}
+                            >
+                                <Alert severity="warning">
+                                    Please fill in all fields before sending
+                                </Alert>
+                            </Snackbar>
+                        </Box>
                     </Box>
-                </Box>
-            </Container>
+                </Container>
+            </FadeInSection>
         </>
     );
 };
